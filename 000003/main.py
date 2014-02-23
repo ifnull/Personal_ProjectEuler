ee35d2b3bed89774af1fdd0455d60bccf2a2c5f3
Daniel Smith <dsmith@blitzagency.com>
2014-02-22 23:13:55 -0800

    Fixes problem #3 √

--- 000003/main.py
+++ 000003/main.py
@@ -1,50 +1,26 @@
 #!/usr/bin/env python
-# Sieve of Eratosthenes
 
-import sys, time
+import sys, math, random, time, decimal
 
-# _max = 600851475143
-_max = 10000
-_cursor = 2 # start at 2. 1 is a factor of everything
-_sieve = {x: True for x in range( _cursor, _max + 1 )}
-_primes = []
+_max = 600851475143
+_winner = 0
 
-def sift():
-  global _cursor, _max
-  next_found = False
-  cursor = count = 0
-
-  for i in range( _cursor + 1, len(_sieve) + 1 ):
-    if _sieve[i] is True:
-      count += 1
-      mod = i % _cursor
-      if mod is 0:
-        _sieve[i] = False
-      elif mod is not 0 and next_found is False:
-        cursor = i
-        next_found = True
-
-  if cursor is not 0:
-    _cursor = cursor
-
-  if count > 0:
-    return True
-  else:
-    return False
+def go():
+  global _max, _winner
+  _cursor = 2
+  while _max > 1:
+    if (_max % _cursor) is 0:
+      _max = _max / _cursor
+      _winner = _cursor
+      _cursor = 2
+    else:
+      _cursor += 1
 
 def main():
   start_time = time.time()
-
-  while sift():
-    pass
-
-  for i,x in enumerate(_sieve):
-    if _sieve[x] is True:
-      _primes.append(x)
-
-  print 'Winner is: ' + str(_primes[-2])
+  go()
+  print 'Winner is: ' + str(_winner)
   print time.time() - start_time, "seconds"
 
 if __name__ == '__main__':
   main()
-
